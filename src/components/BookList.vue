ec730955997efb384dec39c3b49e6cce22782b2d
Marc Greenfield <marc.greenfield@accenture.com>
2020-07-14 11:48:29 -0400

    test

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,11 +4,14 @@
     <ul>
       <book-item v-for='book in books' :book='book'></book-item>
     </ul>
+    <hr/>
+    <book-form @addBook='appendBook'></book-form>
   </div>
 </template>
 
 <script>
 import BookItem from './BookItem';
+import BookForm from './BookForm';
 export default {
   name: 'BookList',
   data() {
@@ -24,7 +27,13 @@ export default {
     };
   },
   components: {
-    BookItem
+    BookItem,
+    BookForm
+  },
+  methods: {
+    appendBook(bookTitle, bookAuthor) {
+      this.books.push({ title: bookTitle, author: bookAuthor });
+    },
   }
 };
 </script>
